8102e5ef8619b446a3cc869f04a0e429dd98cb5e
James Thomas <jabt1998@gmail.com>
2021-05-19 10:52:55 +0100

    added React App

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import styles from './App.module.scss'
+
+const App = () => {
+  return (
+    <h2 className={styles.hello}>Hello</h2>
+  );
+}
+
+export default App;
